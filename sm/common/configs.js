3a911b1c5565770580c70379770bbcabc48d2564
Zhang Jun <123318032@qq.com>
2020-06-02 10:55:34 +0800

    update config

--- sm/common/configs.js
+++ sm/common/configs.js
@@ -1,9 +1,9 @@
 module.export = CONFIG_SM = [
   {
     isbase: true,
-    host: '10.20.37.237',
+    host: 'localhost',
     port: 39100,
-    bases: ['10.20.37.237:39100'],
+    bases: ['localhost:39100'],
     discover: {
       registry: {
         active: true
